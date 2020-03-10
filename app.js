40d059814ac4950a2be7012ca15b046e07e03e8e
mccallumcarson <mccallumcarson@gmail.com>
2020-03-10 15:31:16 -0700

    Add text only confirmation

--- app.js
+++ app.js
@@ -422,15 +422,15 @@ app.post('/sms', (req, res) => {
 });
 
 app.get("/", function (req, res) {
-
+/*
   res.render('pages/index',{users : [
             { name: 'John' },
             { name: 'Mike' },
             { name: 'Samantha' }
   ]});
-
+*/
   //res.sendFile(indexPg);
-  //res.send("SPACE text messaging service is currently operational");
+  res.send("SPACE text messaging service is currently operational");
   //res.sendFile(__dirname + '/index.html');
 });
 
